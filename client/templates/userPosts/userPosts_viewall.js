80e942737ce2d950db9c9d9b58efddb24e65bf2b
zexuanc <syltaris@gmail.com>
2017-08-21 23:28:21 +0800

    changed layout of user's own posts

--- client/templates/userPosts/userPosts_viewall.js
+++ client/templates/userPosts/userPosts_viewall.js
@@ -1,11 +1,6 @@
 
 Template.postsViewAll.helpers({
   userOwnPosts: function() {
-    console.log(UserPosts.find(
-      {
-        authorName: Meteor.user().username,
-      }
-    ).fetch());
     return UserPosts.find(
       {
         authorName: Meteor.user().username,
