690fd6e55616d27c50a6695b534641d72ce8d09a
zexuanc <syltaris@gmail.com>
2017-08-23 01:50:54 +0800

    fix chat user list not redirecting properly

--- client/templates/chat/chatList_item.js
+++ client/templates/chat/chatList_item.js
@@ -1,4 +1,4 @@
-
+// TODO: refactor this...DRY
 Template.chatList_item.helpers({
   otherUserName: function() {
     var theOne = this.participants.find(x => x.userId != Meteor.userId());
@@ -7,4 +7,15 @@ Template.chatList_item.helpers({
   roomIdToGo: function() {
     return this._id;
   },
+  selectedChatUser: function() {
+    var theOne = this.participants.find(x => x.userId != Meteor.userId());
+    return Session.get('selectedChatUser') === theOne.userName ? 'active' : '';
+  },
+});
+
+Template.chatList_item.events({
+  'click .chatUser': function() {
+    console.log('ran');
+    Session.set('selectedChatUser', this.participants.find(x => x.userId != Meteor.userId()).userName);
+  }
 });
