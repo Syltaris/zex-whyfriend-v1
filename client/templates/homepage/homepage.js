10d2268e6579595b2f51c76451d6ff7918b3db41
zexuanc <syltaris@gmail.com>
2017-08-22 10:47:24 +0800

    added colurful toggles for posts views

--- client/templates/homepage/homepage.js
+++ client/templates/homepage/homepage.js
@@ -3,4 +3,45 @@ Template.homepage.helpers({
   allPostsCount: function() {
     return UserPosts.find().count();
   },
+  panel_colour: function() {
+    switch (Session.get('postsView-type')) {
+      case 'locals':
+        return 'panel-default';
+      case 'expats':
+        return 'panel-danger';
+      case 'guides':
+        return 'panel-warning';
+      case 'bowners':
+        return 'panel-success';
+
+      default:
+        return 'panel-default';
+    }
+  },
+  activeClass: function(type) {
+    return type === Session.get('postsView-type') ? 'active' : '';
+  },
+});
+
+Template.homepage.events({
+  'click .all': function(e) {
+    e.preventDefault();
+    Session.set('postsView-type', 'all');
+  },
+  'click .locals': function(e) {
+    e.preventDefault();
+    Session.set('postsView-type', 'locals');
+  },
+  'click .expats': function(e) {
+    e.preventDefault();
+    Session.set('postsView-type', 'expats');
+  },
+  'click .guides': function(e) {
+    e.preventDefault();
+    Session.set('postsView-type', 'guides');
+  },
+  'click .bowners': function(e) {
+    e.preventDefault();
+    Session.set('postsView-type', 'bowners');
+  },
 });
